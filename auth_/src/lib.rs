363599e0d15730d85d93167d70416c6125a2c470
i18n.host <i18n.host@gmail.com>
2025-06-22 19:02:59 +0800

    ♨

--- auth_/src/lib.rs
+++ auth_/src/lib.rs
@@ -8,9 +8,6 @@ pub mod signup;
 use serde::{Deserialize, Serialize};
 use simple_useragent::UserAgentParser;
 
-#[static_init::dynamic]
-pub static UA: UserAgentParser = UserAgentParser::new();
-
 #[derive(Deserialize, Serialize, Debug)]
 pub struct BrowserMeta {
   pub ip: Option<std::net::IpAddr>,
@@ -19,6 +16,25 @@ pub struct BrowserMeta {
   pub os: String,
   pub os_ver: String,
 }
+
+#[static_init::dynamic]
+pub static UA: UserAgentParser = UserAgentParser::new();
+
+// uid: u64,
+// ip: impl AsRef<[u8]>,
+// timezone: i8,
+// dpi: u8,
+// w: u16,
+// h: u16,
+// os_ver: impl AsRef<str>,
+// arch: impl AsRef<str>,
+// cpu_num: u32,
+// gpu: impl AsRef<str>,
+// brand: impl AsRef<str>,
+// os_name: impl AsRef<str>,
+// browser_name: impl AsRef<str>,
+// browser_ver: impl AsRef<str>,
+// browser_lang: impl AsRef<str>,
 
 pub fn test(
   timezone: i8,
@@ -37,5 +53,28 @@ pub fn test(
       .and_then(|v| v.to_str().ok())
       .unwrap_or_default(),
   );
-  dbg!((timezone, dpi, w, h, os_ver, arch, cpu_num, gpu));
+
+  // let os_name = ua.os.name.as_deref().unwrap_or_default();
+  // let brand = ua.device.brand.as_deref().unwrap_or_default();
+  // let browser_name = ua.name.as_deref().unwrap_or_default();
+  // let browser_ver = ua.version.as_deref().unwrap_or_default();
+  let browser_lang = headers
+    .get("accept-language")
+    .and_then(|v| v.to_str().ok())
+    .unwrap_or_default()
+    .split(",")
+    .next()
+    .unwrap_or_default();
+
+  dbg!((
+    timezone,
+    dpi,
+    w,
+    h,
+    os_ver,
+    arch,
+    cpu_num,
+    gpu,
+    browser_lang
+  ));
 }
